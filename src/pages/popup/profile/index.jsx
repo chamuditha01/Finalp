36ec6edbccb78ec585f58f4fc32c936bef834d1a
chamuditha01 <heshanchamuditha05@gmail.com>
2023-09-28 20:17:21 +0530

    rt1

---
+++ src/pages/popup/profile/index.jsx
@@ -0,0 +1,19 @@
+import Doctor from '../../../components/molecules/Doctor';
+import Header from '../../../components/molecules/Header/Header';
+import HeaderPetDashboard from '../../../components/molecules/HeaderPetDashboard/Header';
+import Navbarprofile from '../../../components/molecules/Navbarprofile';
+import imagebak from './cbak1.jpg'
+
+const Service = () => {
+  return (
+    <div>
+     
+      
+        
+        <HeaderPetDashboard/>
+        
+       
+      </div>
+  );
+};
+export default Service;
