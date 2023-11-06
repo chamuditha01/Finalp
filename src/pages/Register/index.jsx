b0fa26de18bd16b12640ecdbdeaa1fbd3995c4ab
Akila Nirmal <akilanirmalzz4352@gmail.com>
2023-11-06 22:37:17 +0530

    register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -68,13 +68,8 @@ const Register = () => {
                   <input type="text" className="form-control" name='lastName' value={formData.lastName}
             onChange={handleChange} id="lastName" required />
                 </div>
-                <div className="col-md-4">
-                  <label htmlFor="validationDefaultUsername" className="form-label">Username</label>
-                  <div className="input-group">
-                    <span className="input-group-text" id="inputGroupPrepend2">@</span>
-                    <input type="text" className="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required />
-                  </div>
-                </div>
+               
+               
                 <div className="col-md-6">
                   <label htmlFor="inputEmail4" className="form-label">Email</label>
                   <input type="email" className="form-control" name='email' value={formData.email}
@@ -90,33 +85,10 @@ const Register = () => {
                   <input type="text" className="form-control" value={formData.address}
             onChange={handleChange} name='address' id="address" placeholder="1234 Main St"  required/>
                 </div>
-                <div className="col-12">
-                  <label htmlFor="inputAddress2" className="form-label">Address 2</label>
-                  <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                </div>
-                <div className="col-md-6">
-                  <label htmlFor="inputCity" className="form-label">City</label>
-                  <input type="text" className="form-control" id="inputCity"  required/>
-                </div>
-                <div className="col-md-4">
-                  <label htmlFor="inputState" className="form-label">State</label>
-                  <select id="inputState" className="form-select" required>
-                    <option selected>Choose...</option>
-                    <option>Central</option>
-                    <option>Eastern</option>
-                    <option>North Central</option>
-                    <option>Northern</option>
-                    <option>North Western</option>
-                    <option>Sabaragamuwa</option>
-                    <option>Southern</option>
-                    <option>Uva</option>
-                    <option>Western</option>
-                  </select>
-                </div>
-                <div className="col-md-2">
-                  <label htmlFor="inputZip" className="form-label">Zip</label>
-                  <input type="text" className="form-control" id="inputZip"  required/>
-                </div>
+              
+                
+                
+               
                 <div className="col-12">
                   <div className="form-check">
                     <input className="form-check-input" type="checkbox" id="gridCheck"required />
